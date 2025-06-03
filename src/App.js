a7fe27f6655d455a7a28eb63da4dbcd26bcf1e5a
Sandun <sandunhhh90@gmail.com>
2025-06-03 19:52:47 +0530

    debugged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import NavigationButton from './components/NavigationButton';
 import AboutPage from './components/AboutPage';
